51f0d7512284a3cc2fa011c26204063a78b20220
Kurdyk <louiskurdyk@gmail.com>
2020-04-19 19:54:14 +0200

    En cours, bug sur l'age des entites et l'ajout de nourriture'

--- src/Code.py
+++ src/Code.py
@@ -1,6 +1,7 @@
 
 
 dict_ex = {"x" : 0, "y": 0, "contenu" : 1, "dernier_repas" : 0, "age" : -1, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 0}
+case_nourriture = {"x" : 0, "y": 0, "contenu" : 1, "dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 1}
 case_vide = {"x" : 0, "y": 0, "contenu" : 0, "dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 1}
 case_antibio = {"x" : 0, "y": 0, "contenu" : 2, "dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 1}
 
@@ -36,11 +37,11 @@ def pause():
 
 #widget ph_opt, temp_opt, taux de croissance optimal... enfin tous les parametres mis en demande a l'utilisateur pour deux bacteries donc on indice les valeur par le type 1 ou 2 de la bacterie (ex : ph_opt2) sauf pour la temperature de la boite notee temp et son ph note ph qui prennent d'autres curseurs
 
-lifespawn_bact1 = 200
-lifespawn_bact2 = 200
-lifespawn_sans_nourriture_bact1 = 100
-lifespawn_sans_nourriture_bact2 = 100
-lifespawn_antibio = 100
+lifespawn_bact1 = 20
+lifespawn_bact2 = 1
+lifespawn_sans_nourriture_bact1 = 10
+lifespawn_sans_nourriture_bact2 = 5
+lifespawn_antibio = 200
 
 def taux_de_croissance_effectif(taux_opt, ph, ph_opt, temp, temp_opt):
     if temp_opt <= 20:
@@ -195,21 +196,27 @@ def in_liste(contenu, liste): #cherche si un contenu est dans une liste de dico
 
 def mort_bacterie(boite, x, y):
     box = boite
-    box[x][y] = {"x" : x, "y": y, "contenu" : 1, 'dernier_repas' : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, 'capa_de_repro' : 0, 'nb_action' : 1}
+    box[x][y] = {"x" : x, "y": y, "contenu" : 0, 'dernier_repas' : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, 'capa_de_repro' : 0, 'nb_action' : 1}
+    return box
+
+def mort_antibio(boite, x, y):
+    box = boite
+    box[x][y] = {"x" : x, "y": y, "contenu" : 0, 'dernier_repas' : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, 'capa_de_repro' : 0, 'nb_action' : 1}
     return box
 
 def deplacement_possible(boite, x, y):
     if boite[x][y]['contenu'] == 3 or boite[x][y]['contenu'] == 4:
         if boite[x][y]['nb_action'] != 0:
             return False
-        voisinage_direct = voisinage(boite, x, y, 1)
-        for i in range(0, len(voisinage_direct)):
-            if (voisinage_direct[i]['contenu'] == 0 or voisinage_direct[i]['contenu'] == 1) or (boite[x][y]['resistance'] != -1 and voisinage_direct[i]['contenu'] == 2):
+        if len(voisinage_direct_libre(boite, x, y)) > 0:
                 return True
     return False
 
 def distance(x0, y0, x1, y1):
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
+def distance_parcours(x0, y0, x1, y1):
+    return abs(x1 - x0) + abs(y1 - y0)
 
 def nourriture_plus_proche(boite, x, y, taille):
     voisinage0 = voisinage(boite, x, y, taille)
@@ -236,20 +243,42 @@ def chemin_plus_rapide(x0, y0, x1, y1):
     """Renvoie le chemin le plus rapide de (x0, y0) à (x1, y1) dans un tuple de deplacements à effectuer"""
     return (x1 - x0, y1 - y0)
 
-def deplacement(boite, x, y, new_x, new_y):
+def deplacement(boite, x, y, coordonnees):
+    new_x, new_y = coordonnees
     box = boite
     if box[new_x][new_y]['contenu'] == 1:
         box[x][y]['dernier_repas'] = 0
     box[new_x][new_y] = box[x][y]
     box[new_x][new_y]['nb_action'] = 1
-    box[x][y] = {"x" : 0, "y": 0, "contenu" : 0,"dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 1} #laisse un vide après s'être déplacée
+    box[x][y] = {"x" : x, "y": y, "contenu" : 0,"dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 1} #laisse un vide après s'être déplacée
     return box
 
-def naissance(boite, x, y, new_x, new_y):
+def voisinage_direct_libre(boite, x, y):
+    voisinage0 = voisinage(boite, x, y, 1)
+    res = []
+    for i in range(0, len(voisinage0)):
+        case = voisinage0[i]
+        if distance_parcours(x, y, case["x"], case['y']) == 1 and ( (case['contenu'] == 0 or case['contenu'] == 1) or (boite[x][y]['resistance'] != -1 and case['contenu'] == 2) ):
+            res.append(case)
+    return res
+
+def choix_deplacement(boite, x, y, taille):
+    if deplacement_possible(boite, x, y):
+        box = boite
+        voisi = voisinage_direct_libre(box, x ,y)
+        choix = voisi[random.choice(range(0, len(voisi)))]
+        x = choix["x"]
+        y = choix["y"]
+        return (x, y)
+    else:
+        return None
+
+def naissance(boite, x, y, coordonnees):
+    new_x, new_y = coordonnees
     box = boite
     bact_type = boite[x][y]['contenu']
-    box = deplacement(boite, x, y, new_x, new_y)
-    box[x][y] = {"x" : 0, "y": 0, "contenu" : bact_type,"dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, "nb_action" : 1} #laisse une nouvelle bactérie après s'être déplacée
+    box = deplacement(boite, x, y, coordonnees)
+    box[x][y] = {"x" : x, "y": y, "contenu" : bact_type,"dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : box[new_x][new_y]['taux_de_croissance'], "capa_de_repro" : 0, "nb_action" : 1} #laisse une nouvelle bactérie après s'être déplacée
     box[new_x][new_y]['capa_de_repro'] = 0
     return box
 
@@ -268,13 +297,14 @@ def remplace_par_antibio_si_non_resis(boite, x, y):
         box[x][y]['y'] = y
     return box
 
-def ajout_antibio(boite, x, y):
+def ajout_antibio(boite, coordonnees):
     """je suppose qu'on prend 100 * 100 en format de boite pour faire l'echelle de la goute"""
     box = boite
+    x, y = coordonnees
     for i in range(-4, 5):
         for j in range(-4, 5):
-            if distance(x, y, x + i, y + j) <= 5 and ( ( (i == -1) or (i == 1) ) and ( (j == -4) or (j == 4) ) ):
-                remplace_par_antibio_si_non_resis(box, x + i, y + j)
+            if distance_parcours(x, y, x + i, y + j) <= 5 and ( ( (i != -1) or (i != 1) ) and ( (j != -4) or (j != 4) ) ):
+                box = remplace_par_antibio_si_non_resis(box, x + i, y + j)
     return box
 
 def evo_age(boite):
@@ -282,7 +312,7 @@ def evo_age(boite):
     for i in range(0, len(box)):
         for j in range(0, len(box[0])):
             contenu = box[i][j]['contenu']
-            if contenu == 2 or contenu == 3 or contenu == 4:
+            if contenu >= 2:
                 box[i][j]['age']+= 1
                 if contenu != 2:
                     box[i][j]['dernier_repas']+= 1
@@ -297,6 +327,8 @@ def evo_capa_de_reprod(boite):
                 box[i][j]['capa_de_repro']+=0.1
     return box
 
+
+
 def death_verif_and_apply(boite):
     box = boite
     for i in range(0, len(box)):
@@ -304,8 +336,9 @@ def death_verif_and_apply(boite):
             contenu = box[i][j]['contenu']
             age = box[i][j]['age']
             dernier_repas = box[i][j]['dernier_repas']
-            if contenu == 2 and age >= lifespawn_antibio:
-                box[i][j] = {"x" : x, "y": y, "contenu" : 0, 'dernier_repas' : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, 'capa_de_repro' : 0, 'nb_action' : 1}
+            if contenu == 2 and age >= 200:
+                print(box[i][j]['x'], box[i][j]['y'])
+                mort_antibio(boite, i, j)
             if contenu == 3 and (age >= lifespawn_bact1 or dernier_repas >= lifespawn_sans_nourriture_bact1):
                 box = mort_bacterie(box, i, j)
             if contenu == 4 and (age >= lifespawn_bact2 or dernier_repas >= lifespawn_sans_nourriture_bact2):
@@ -318,6 +351,40 @@ def reset_nb_action(boite):
         for j in range(0, len(box[0])):
             box[i][j]['nb_action'] = 0
     return box
+
+def coo_vide(boite):
+    res = []
+    for i in range(0, len(boite)):
+        for j in range(0, len(boite[0])):
+            case = boite[i][j]
+            if case["contenu"] == 0:
+                res.append((case['x'], case['y']))
+    return res
+
+def coo_vide_nourriture(boite, demi_cote):
+    res = []
+    dim = len(boite)
+    for i in range(0, len(boite)):
+        for j in range(0, len(boite[0])):
+            case = boite[i][j]
+            if case["contenu"] == 0 and i + demi_cote < dim and i - demi_cote >= 0 and j + demi_cote < dim and j - demi_cote >= 0:
+                res.append((case['x'], case['y']))
+    return res
+
+def pos_alea_nourriture(boite, demi_cote):
+    liste = coo_vide_nourriture(boite, demi_cote)
+    return liste[random.choice(range(0, len(liste)))]
+
+def ajout_nourriture(boite, demi_cote, coordonnees):
+    box = boite
+    x, y = coordonnees
+    for i in range(-demi_cote, demi_cote + 1):
+        for j in range(-demi_cote, demi_cote + 1):
+            case = box[x + i][y + j]
+            if case["contenu"] == 0:
+                case = {"x" : x + i, "y": y + j, "contenu" : 1, "dernier_repas" : 0, "age" : 0, "resistance" : -1, "taux_de_croissance" : -1, "capa_de_repro" : 0, 'nb_action' : 1}
+    return box
+
 
 ### Affichage
 def convert_to_array(boite):
@@ -338,36 +405,67 @@ def plot_boite(boite):
     plt.tick_params(top=False, bottom=False, right=False, left=False, labelleft=False, labelbottom=False)
     plt.show()
 
+### Statistiques
+#pas mal de fonctions proches des stats de Schelling
+def nombres_individus(boite):
+    res = {"Bacterie1" : 0, "Bacterie2" : 0}
+    for i in range(0, len(boite)):
+        for j in range(0, len(boite[0])):
+            contenu = boite[i][j]['contenu']
+            if contenu == 3:
+                res["Bacterie1"]+=1
+            if contenu == 4:
+                res["Bacterie2"]+=1
+    return res
+
+
 ###Global
+def continuer(boite):
+    pop = nombres_individus(boite)
+    if pop["Bacterie1"] + pop["Bacterie2"] == 0:
+        return False
+    return True
 
 def tour(boite):
     box = boite
     for i in range(0, len(box)):
         for j in range(0, len(box[0])):
             if deplacement_possible(box, i, j):
-                a, b = nourriture_plus_proche(box, i,j, 1)
+                dest = choix_deplacement(box, i, j, 1)
                 case = box[i][j]
-                print(case)
                 if case['age'] >= 1 and case['capa_de_repro'] >= (1 - case['taux_de_croissance']):
-                    box = naissance(box, i, j, a, b)
+                    box = naissance(box, i, j, dest)
                 else:
-                    box = deplacement(box, i, j, a, b)
+                    box = deplacement(box, i, j, dest)
     box = evo_age(box)
     box = death_verif_and_apply(box)
     box = evo_capa_de_reprod(box)
     box = reset_nb_action(box)
     return box
 
+def ajout(boite, n_iter, f_antibio, f_nourriture, demi_cote, debut_ajout):
+    box = boite
+    if n_iter >= debut_ajout and n_iter % f_antibio == 0:
+        box = ajout_antibio(boite, pos_alea_antibio(box))
+        plot_boite(box)
+    if n_iter >= debut_ajout and n_iter % f_nourriture == 0 and coo_vide_nourriture(box, demi_cote) != [] :
+        box = ajout_nourriture(box, demi_cote, pos_alea_nourriture(box, demi_cote))
+    return box
+
 def structure_bacterio(iter_max):
     temp = 25
     ph = 7
-    box = init_boite(10)
+    box = init_boite(100)
     init_pos_bact(box, init_bact1(1, ph, 7, temp, 25))
     cmpt = 0
+    f_antibio = 5
+    f_nourriture = 1
+    demi_cote = 15
+    debut_ajout = 10
     plot_boite(box)
-    while cmpt < iter_max:
+    while cmpt < iter_max and continuer(box):
         box = tour(box)
-        plot_boite(box)
+        box = ajout(box, cmpt, f_antibio, f_nourriture, demi_cote, debut_ajout)
         cmpt+=1
-        #pause()
+    plot_boite(box)
     return None
